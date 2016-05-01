adf6f85bdc666e873e9a337d4dd63b67ba76d5c0
Roee Gilron <roeegilron@gmail.com>
2016-05-01 15:06:14 +0300

    fix

--- scoringToMatrix1DansMat.m
+++ scoringToMatrix1DansMat.m
@@ -1,5 +1,6 @@
 function data3d = scoringToMatrix1DansMat(mask, data, locations)
 % this function takes 2D data and returns 3D data 
+data3d = zeros(size(mask));
 for i = 1:size(data,1)
     data3d(locations(i,1),locations(i,2),locations(i,3)) = data(i,1);
 end
